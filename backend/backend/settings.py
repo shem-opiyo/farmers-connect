faca8bce67aac867c073cd0fbab6bb3fada04380
shem-opiyo <shemnicholasopiyo@gmail.com>
2025-09-29 08:15:59 +0300

    updated cors settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,11 +48,11 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 
-CORS_ALLOWED_ORIGINS = True
-# [
-#      "http://localhost:5173",
-#      "http://127.0.0.1:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+     "http://localhost:5173",
+     "http://127.0.0.1:5173",
+     "https://farmerconnekt.netlify.app/",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
